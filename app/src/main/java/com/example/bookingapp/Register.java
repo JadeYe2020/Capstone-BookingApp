621acb29ba3f4ec256f8c6d344e902df80e170ce
Jade Ye <JadeYe2020>
2022-03-07 15:54:57 -0400

    Change made by CK: Add one EditText for password confirmation when register.

--- app/src/main/java/com/example/bookingapp/Register.java
+++ app/src/main/java/com/example/bookingapp/Register.java
@@ -24,7 +24,7 @@ import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    EditText et_name, et_password, et_email;
+    EditText et_name, et_password, et_email, et_confirm_password;
     Button btn_register, btn_go_login;
     boolean validInput =true;
     FirebaseAuth fbAuth;  //this is for authentication
@@ -44,6 +44,7 @@ public class Register extends AppCompatActivity {
         et_email = findViewById(R.id.et_email);
         btn_register = findViewById(R.id.btn_register);
         btn_go_login = findViewById(R.id.btn_go_login);
+        et_confirm_password = findViewById(R.id.et_confirm_password);
 
         //go to the login page
         btn_go_login.setOnClickListener(new View.OnClickListener() {
@@ -61,42 +62,49 @@ public class Register extends AppCompatActivity {
                 checkForInput(et_email);
                 checkForInput(et_password);
 
-                //if valid, begin registering user
-                if(validInput){
+                if(et_password.getText().toString().equals( et_confirm_password.getText().toString())){
 
-                    //creates the new user with email and password
-                    fbAuth.createUserWithEmailAndPassword(et_email.getText().toString(),
-                            et_password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            //This gets triggered when the user successfully creates account
-                            Toast.makeText(Register.this, "Account Created", Toast.LENGTH_LONG).show();  //message to user
+                    //if valid, begin registering user
+                    if(validInput){
 
-                            FirebaseUser user = fbAuth.getCurrentUser(); //get the info of newly created user
-                            DocumentReference docRef = fbStore.collection("Users").document(user.getUid());  //get user id
+                        //creates the new user with email and password
+                        fbAuth.createUserWithEmailAndPassword(et_email.getText().toString(),
+                                et_password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                //This gets triggered when the user successfully creates account
+                                Toast.makeText(Register.this, "Account Created", Toast.LENGTH_LONG).show();  //message to user
 
-                            //select data to store
-                            Map<String, Object> userInfo = new HashMap<>();
-                            userInfo.put("Name", et_name.getText().toString());
-                            userInfo.put("Email", et_email.getText().toString());
+                                FirebaseUser user = fbAuth.getCurrentUser(); //get the info of newly created user
+                                DocumentReference docRef = fbStore.collection("Users").document(user.getUid());  //get user id
 
-                            //determine if user is admin
-                            userInfo.put("isUser", "1"); //normal user, non admin
+                                //select data to store
+                                Map<String, Object> userInfo = new HashMap<>();
+                                userInfo.put("Name", et_name.getText().toString());
+                                userInfo.put("Email", et_email.getText().toString());
 
-                            docRef.set(userInfo); //save to DB
+                                //determine if user is admin
+                                userInfo.put("isUser", "1"); //normal user, non admin
+
+                                docRef.set(userInfo); //save to DB
 
 
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));  //currently sending everyone to main
 
-                            finish(); // this stops users from going back to register activity
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(Register.this, "Failed to Create Account", Toast.LENGTH_LONG).show();
-                        }
-                    });
-                }//end if
+                                finish(); // this stops users from going back to register activity
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Register.this, "Failed to Create Account", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }//end if
+                }else{
+                    Toast.makeText(Register.this, "Passwords do not match, try again", Toast.LENGTH_LONG).show();
+                }
+
+
             }
         }); //end on click
     }
